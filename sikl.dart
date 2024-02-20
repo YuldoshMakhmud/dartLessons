ffb7f40e52fb3e1712ceae54f8fa3cd58dcffe36
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-02-21 01:41:48 +0900

    lesson

--- sikl.dart
+++ sikl.dart
@@ -1,3 +1,100 @@
-void main(List<String> args) {
+// void main(List<String> args) {
+  // oddiy holattada
+  // int i = 1;
+  // i += 6;
+  // print(i);
+
+// for bilan
+  // for (var i = 0; i < 10; i++) {
+  //   print("hello 10ta");
+  // }
+// }
+
+
+// ====================================================================
+
+// sikl   List bilan
+
+// void main(List<String> args) {
+//   List<String> fruits = ["apple","orange","banana","kiwi"];
+
+// int uzunligi = fruits.length;
+// for (int i = 0; i<uzunligi; i++){
+//   print(fruits[i]);
+// }
+// }
+//====================================================================
+
+// // if bilan
+// void main(List<String> args) {
+//   for(int i = 1; i<=10; i++){
+//     if(i % 2 == 0){
+//     print("i juft son"+ i.toString());
+//   } else{
+//     print("i toqson"+ i.toString());
+//   }
+// }
+// }
+//=====================================================================
+
+
+//sikllarda  break and continue
+
+// void main(List<String> args) {
+//   print("sikl boshlandi");
+
+// for(int i =0; i<=5; i++){
+//   if(i == 4){
+//     break;
+//   }
+//   print("hozirgi qiymat"+ i.toString());
+// }
+
+// // conteniu
+// for(int i =0; i<=5; i++){
+//   if(i == 4){
+//     continue;
+//   }
+//   print("hozirgi qiymat"+ i.toString());
+// }
+
+//   print("sikl tugadi");
   
-}
+// }
+
+// ==============================================
+
+// while , do while
+
+// void main(List<String> args) {
+//   int i = 0;
+//   while(i <5)
+//   {  i++;
+//     print("hello " + i.toString());
+//   }
+// }
+
+//dowhile  birinchi siklni bajaradi keyin shartni shart rost bulsa yana bajarib quyadi  
+
+// void main(List<String> args) {
+//   int i = 100;
+//   do{
+//     //sikl code
+//     i++;
+//     print(i);
+//   }while(i<10);
+// }
+
+//==============================================================================================
+
+// ichma ich sikllar matritsalar
+// void main(List<String> args) {
+//   List<int> list = [1,2,3];      // bir ulchamli matrixsa
+//   List<List<int>> matrix = [                    //  ikki ulchamli matrix yani   list ichida listlar bor degani
+//     [1,2,3],
+//     [2,3,4],
+//     [2,3,4],
+//     [2,3,4],
+//     [2,3,4],
+//   ];
+//   }
