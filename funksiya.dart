59da46b1a2fde5854608e596d246f713cd1127ac
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-03-23 03:57:30 +0900

    lessson

--- funksiya.dart
+++ funksiya.dart
@@ -2,11 +2,13 @@
   void myFunction(int a ,int b){      // void qiymat qaytarmaydigin function
     print(a+b);
   }
-  myFunction1(){
-    print("flutter");
-  }
 
+  int myFunction1(int n1, int n2){
+      return (n1 + n2);
+  }
 
 void main(List<String> args) {
   myFunction(2,2);
+  int result = myFunction1(4,4);
+  print(result);
 }
