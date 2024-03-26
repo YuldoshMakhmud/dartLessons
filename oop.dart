a0a8fb18c1d96c3fda498ad9fc6a2c423b78d010
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-03-26 10:02:12 +0900

    lesson

--- oop.dart
+++ oop.dart
@@ -1,20 +1,53 @@
+
 class Car {
   String? name;
   int? km;
   bool? isElectroCar;
   String? color;
 
-  Car(String Carname, int Carkm, bool CarisElectroCar,String Carcolor) {
-    this.name = Carname;
-    this.km = Carkm;
-    this.isElectroCar = CarisElectroCar;
-    this.color =  Carcolor;
+// named constructor
+Car.named(this.name, this.km, this.isElectroCar): color= "white"; // 2ta nuqta daefoult rang beradi null chiqmaslik uchun
+
+
+// short form 
+Car(this.name, this.km, this.isElectroCar, this.color);
+
+
+// long form constructor
+  // Car(String Carname, int Carkm, bool CarisElectroCar,String Carcolor) {
+  //   this.name = Carname;
+  //   this.km = Carkm;
+  //   this.isElectroCar = CarisElectroCar;
+  //   this.color =  Carcolor;
+  // }
+
+                        // methods
+  void start(){
+    print("${this.name} ingane start");
   }
+  void stop(){
+    print("${this.name} ingane stop");
+  }
+  void parking(){
+    print("${this.name}  parking mode");
+  }
+
 }
 void main(List<String> args){
   Car Gentra = Car("gentra", 2400, false, "white");
-  print(Gentra.name);
-  print(Gentra.km);
-  print(Gentra.isElectroCar);
-  print(Gentra..color);
+  // print(Gentra.name);
+  // print(Gentra.km);
+  // print(Gentra.isElectroCar);
+  // print(Gentra.color);
+
+  Car Matiz = Car.named("matiz", 10000, false);
+  Car Malibu = Car("Malibu", 2000, false, "green");
+  Car Ionic = Car("Ionic 5", 4000, true, "black");
+
+  Gentra.start();
+  Matiz.start();
+  Malibu.stop();
+  Ionic.parking();
+
 }
+
