62f84aca7e3a81cf7cefa458a85dfc3d2d06a01d
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-04-02 08:37:06 +0900

    json

--- class1.dart
+++ class1.dart
@@ -1,15 +1,35 @@
 // override
 
-class Car{
+// class Car{
+//   String name;
+//   String color;
+
+//   Car(this.name,this.color);
+
+
+//   @override
+
+//   String toString(){
+//     return "Car name: $name\nCar's color: $color";
+//   }
+// }
+
+class Person{
   String name;
-  String color;
+  int   age;
+  String location;
 
-  Car(this.name,this.color);
+  Person(this.name,this.age,this.location);
 
+     Person fromJson(Map<String,dynamic>json){
+      String name = json['name'];
+      String location =json['location'];
+      int age = json['age'];      
+      return Person(name, age, location);
+    }
 
-  @override
-
-  String toString(){
-    return "Car name: $name\nCar's color: $color";
-  }
+    @override
+    String toString(){
+      return "Name : $name\nage: $age\nlocation: $location";
+    }
 }
--- class2.dart
+++ class2.dart
@@ -2,7 +2,18 @@ import 'class1.dart';
 
 
 void main(){
+//   Car malibu = Car("malibu","black");
+//   print(malibu);
 
-  Car malibu = Car("malibu","black");
-  print(malibu);
-}
+//json 
+  Map<String,dynamic> json={
+    'name': 'Makhmud',
+    'age' : 28,
+    'location' : "daegu"
+
+
+  };
+ Person person=Person("ddd", 13, "sads");
+ Person newPerson=person.fromJson(json);
+ print(newPerson);
+ }
