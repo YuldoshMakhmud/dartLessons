d2a23202ebb7922de340949857ee716cdfb4e2ee
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-04-05 01:42:59 +0900

    funksiya

--- funksiya.dart
+++ funksiya.dart
@@ -6,11 +6,16 @@
   int myFunction1(int n1, int n2){
       return (n1 + n2);
   }
-
+void getMilk(int bottles){
+  double  cost = 2 * 1.5;
+  print("liter $cost and $bottles");
+}
   
 
 void main(List<String> args) {
+getMilk(2);
   myFunction(2,2);
   int result = myFunction1(4,4);
   print(result);
+  print(getMilk);
 }
