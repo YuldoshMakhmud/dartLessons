967afd997c9316a29e35110aea55cba5e23cb751
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-04-13 08:44:19 +0900

    lessons

--- input.dart
+++ input.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 void main(){
-print("please type your name");
-String? name = stdin.readLineSync(); // from user  get info
-print('salom $name');
+// print("please type your name");
+// String? name = stdin.readLineSync( ); // from user  get info
+// print('salom $name');
+dynamic person = stdin.readLineSync();
+
+if(person== "makhmud"){
+  print(" your id $person and true get licence");
+
+}else{
+  print("your id $person wrong  please check good luck");
+}
 }
