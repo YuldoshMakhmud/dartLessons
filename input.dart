bb3477c17ccf117a04cb88332a924ddc9bef52aa
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-04-13 07:53:45 +0900

    lessons

---
+++ input.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+void main(){
+print("please type your name");
+String? name = stdin.readLineSync(); // from user  get info
+print('salom $name');
+}
--- veriable.dart
+++ veriable.dart
@@ -1,12 +1,25 @@
-void main(List<String> args) {
-  // Veriables 
-  int a = 32; // int   son uchun uzgaruvchi
-  String b = "MAX"; // MALUMOT UCHUN
-  bool c = true; //  boolean qiymat uchun
-  //  ==   tengmi
-  // != teng emasmi
-  // > kattami
-  // < kichikmi
-  // >== latta yoki teng
-  //<= kichik yoki teng
+void main(){
+  // variables
+// String 
+// int 
+// double
+// num
+//bool 
+
+
+//var  
+
+// dynamic   
+String name= "makhmud";
+int age = 28;
+num height = 1.76;
+double size = 265.5;
+bool isMarried = true;
+
+print("my name is $name");
+print("Age is $age");
+print("are you married $isMarried");
+
+
+
 }
